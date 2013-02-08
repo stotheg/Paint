3222506ba5c859aba5f857713a84e89083eb975d
Lukas Gianinazzi <orionweissnix@gmail.com>
2013-02-08 15:56:17 +0100

    Updated for 1st assignment

--- Paint/PaintingViewController.m
+++ Paint/PaintingViewController.m
@@ -9,57 +9,9 @@
 #import "PaintingViewController.h"
 #import "ColorPickerView.h"
 
-@interface PaintingViewController ()
-
-
-
-@end
-
 
 @implementation PaintingViewController
 
-
-- (void)addSquareWithWidth:(float)width height:(float)height color:(UIColor*)color startPoint:(CGPoint)bottomLeft
-{
-    
-    [self.paths startPathAtPoint:bottomLeft color:color width:2];
-    [self.paths extendLastPathByMovingX:width movingY:0];
-    [self.paths extendLastPathByMovingX:0 movingY:-height];
-    [self.paths extendLastPathByMovingX:-width movingY:0];
-    [self.paths extendLastPathByMovingX:0 movingY:height];
-    
-}
-
-
-- (void)addRowOfBricks:(int)numberOfBricks widthOfBricks:(float)brickWidth heightOfBricks:(float)brickHeight color:(UIColor*)color startPoint:(CGPoint)startPoint
-{
-    CGPoint squareOrigin = startPoint;
-    
-    for (int i = 0; i < numberOfBricks; i++) {
-        
-        [self addSquareWithWidth:brickWidth height:brickHeight color:color startPoint:squareOrigin];
-        
-        squareOrigin.x = squareOrigin.x + brickWidth;
-        
-    }
-    
-}
-
-
-- (void)addPyramidWithNumberOfBricksInBase:(int)numberOfBricks widthOfBricks:(float)brickWidth heightOfBricks:(float)brickHeight color:(UIColor*)color startPoint:(CGPoint)startPoint
-{
-    CGPoint startPointForRow = startPoint;
-    
-    for (int i = 0; i < numberOfBricks; i++) {
-        
-        [self addRowOfBricks:numberOfBricks-i widthOfBricks:brickWidth heightOfBricks:brickHeight color:color startPoint:startPointForRow];
-        
-        startPointForRow.y = startPointForRow.y - brickHeight;
-        startPointForRow.x = startPointForRow.x + brickWidth/2;
-        
-    }
-    
-}
 
 
 #pragma mark lifecycle
@@ -71,11 +23,10 @@
     //call the superclass implementation
     [super viewWillAppear:animated];
     
-    self.canvas.backgroundColor = [UIColor purpleColor];
+    //call your drawing method(s) here:
+    //...
     
-    //custom drawing
-    CGPoint startPoint = CGPointMake(38, 290);
-    [self addPyramidWithNumberOfBricksInBase:8 widthOfBricks:30 heightOfBricks:10 color:[UIColor blueColor] startPoint:startPoint];
+    //update the view
     [self.canvas setNeedsDisplay];
 
 }
